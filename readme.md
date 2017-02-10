64aefebd441de5cc15ac0c3ec33a9ea8a5f424de
Shayne Meyer <shayne.meyer@gmail.com>
2017-02-09 16:01:51 -0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# SoundClound React Client
+
+* [https://www.robinwieruch.de/the-soundcloud-client-in-react-redux/](https://www.robinwieruch.de/the-soundcloud-client-in-react-redux/)
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
+import Stream from './components/Stream/Stream';
+
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some other track'
+  }
+];
+
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
+
+ReactDOM.render(
+  <Provider>
+    <Stream />
+  </Provider>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
